7ccbe6727c92a0356802e794402cb1be49cbbf3b
Mason Hall <masonhall@gmail.com>
2018-09-20 03:21:32 -0500

    modding main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 import networkx as nx
 import random
 from give import *
+
+style.use('fivethirtyeight')
 
 G = nx.path_graph(3)
 
@@ -11,16 +15,18 @@ G.nodes[2]['value'] = 4
 
 # G = nx.Graph()
 # G.add_edges_from([(-2, -1), (-1, 4)])
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+def animate(i, G) :
+    ax1.clear()
+    nx.draw_networkx(G,labels=G.nodes)
+    ax1.plot()
 
 node_num = -1
+# plt.ion()
+# nx.draw_networkx(G,labels=G.nodes)
+# plt.cla()
 while node_num != 'exit' :
-    print(G.nodes)
-    # nx.draw_networkx_labels(G,G.graph)
-    nx.draw(G)
-
-    # labels (dictionary, optional (default=None)) – Node labels in a dictionary keyed by node of text labels
-    nx.draw_networkx(G,labels=G.nodes)
-    plt.show()
+    ani = animation.FuncAnimation(fig,animate, interval=1000)
     node_num = input('Which node do you want give money from?')
-
     give(G, int(node_num))
